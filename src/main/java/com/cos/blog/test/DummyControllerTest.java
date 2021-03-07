314f04908707f8cef49032d0ec4943e8e0a8ab36
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-03-07 18:23:55 +0900

    전체 select 및 paging 테스트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +17,30 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+
 @RestController
 public class DummyControllerTest {
 
 	@Autowired	// 의존성 주입
 	private UserRepository userRepository;
+	
+	// 전체 select
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	
+	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		
+//		Page<User> users = userRepository.findAll(pageable);
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
 	
 	// {id}주소로 파라미터를 전달 받을 수 있음
 	@GetMapping("/dummy/user/{id}")
