9398e467ae3a2edb82b0230e23990bc79e4f4877
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-03-07 18:45:58 +0900

    update 테스트save함수 익힘

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -23,6 +27,29 @@ public class DummyControllerTest {
 
 	@Autowired	// 의존성 주입
 	private UserRepository userRepository;
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		//json 데이터 요청=>  MessageConverter의 Jackson라이브러리가 변환해서 받아준다!
+		System.out.println("id: "+id);
+		System.out.println("password: "+requestUser.getPassword());
+		System.out.println("password: "+requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+//		save함수는 id를 전달하지 않으면 insert를 해주고
+//		save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해줌!
+//		userRepository.save(requestUser);	
+//		더티 채킹
+		
+		return null;
+		
+	}
+	
 	
 	// 전체 select
 	@GetMapping("/dummy/users")
