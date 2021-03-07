549891ee6d0affa7d66fcbb2c5513fd69c42430f
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-03-07 16:54:53 +0900

    id로 select 및 findById -> orElseThrow 및 람다식

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,28 @@ public class DummyControllerTest {
 
 	@Autowired	// 의존성 주입
 	private UserRepository userRepository;
+	
+	// {id}주소로 파라미터를 전달 받을 수 있음
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/7같으거 던지면 user가 null되서 반환되니! orElseThrow 사용!
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id:"+id);
+			}
+		});
+		
+		// 람다식
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//				return new IllegalArgumentException("해당 유저는 없습니다. id:"+id);
+//		});
+		
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// user 오브젝트를 json으로 변환해서 브라우저에 응답해준다!
+		return user;
+	};
 	
 	@PostMapping("/dummy/join")
 	public String join(User user) {		// key=value
